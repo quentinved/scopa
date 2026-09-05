f40acd4565838829fd1c6ed4860e5ce49758155d
Quentin Vedrenne <quentin.vedrenne@epitech.eu>
2026-09-05 02:17:14 +0200

    Initial Commit

---
+++ le_balai/le_balaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  le_balaiApp.swift
+//  le_balai
+//
+//  Created by Quentin Vedrenne on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct le_balaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
